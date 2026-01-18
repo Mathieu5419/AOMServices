cd6b424d9dd1ac30a2c6c8a05621a22ed25cfab2
mathieu5419 <mathieuaina@gmail.com>
2026-01-18 18:30:04 +0300

    javascript

---
+++ script/index.js
@@ -0,0 +1,17 @@
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(`Hello, ${firstName} ${lastName}!`);
+
+let age = 30;
+
+age = 25;
+
+const birthYear = 1999;
+
+// birthYear = 2000; // This will cause an error
+
+console.log(`You are ${age} years old.`);
+console.log(`You were born in ${birthYear}.`);
+
+console.log((3 + 2) - 76 * (1 + 1));
